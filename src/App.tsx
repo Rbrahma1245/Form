45c375ee474df121e07ec5e858489585db23de91
unknown <Rbrahma1245@gmail.com>
2023-02-12 22:02:36 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import RegistrationForm from "./RegistrationForm";
+
+
+function App() {
+
+  return (
+    <div className="App">
+
+        <RegistrationForm />   
+    </div>
+  );
+}
+export default App;
